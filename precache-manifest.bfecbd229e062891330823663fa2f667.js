a117f3a3dce93ead8c8ee6ac4da6fd1a3c2aff8d
Kaya0312 <oyama_kayano@yahoo.co.jp>
2021-01-08 11:17:20 +0900

    Updates

---
+++ precache-manifest.bfecbd229e062891330823663fa2f667.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119cf9ee4be97632bef4fe4b09657e18",
+    "url": "/digiqlo/index.html"
+  },
+  {
+    "revision": "90f399f0329a532f06cf",
+    "url": "/digiqlo/static/css/main.6fd25d42.chunk.css"
+  },
+  {
+    "revision": "a4db11fa0de60f9ba74f",
+    "url": "/digiqlo/static/js/2.b2349276.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/digiqlo/static/js/2.b2349276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f399f0329a532f06cf",
+    "url": "/digiqlo/static/js/main.79e197d8.chunk.js"
+  },
+  {
+    "revision": "ec67a452d562d5db72be",
+    "url": "/digiqlo/static/js/runtime-main.0e1fe0e1.js"
+  }
+]);
